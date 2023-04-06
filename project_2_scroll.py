359891eebd1ba5fdb297a932b6ce8234bf9c3d3b
MargoMorugina <mv.morugins@gmail.com>
2023-04-06 12:32:40 +0200

    task for execute_script

---
+++ project_2_scroll.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+try:
+    link = "http://suninjuly.github.io/execute_script.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    def calc(x):
+        return str(math.log(abs(12 * math.sin(int(x)))))
+
+    x_element = browser.find_element(By.CSS_SELECTOR, '#input_value')
+    x = x_element.text
+    y = calc(x)
+
+    # ввести ответ в текстовое поле
+    input1 = browser.find_element(By.CSS_SELECTOR, '#answer')
+    input1.send_keys(y)
+
+    option1 = browser.find_element(By.CLASS_NAME, "form-check-input")
+    option1.click()
+
+    option2 = browser.find_element(By.CSS_SELECTOR, "#robotsRule")
+    browser.execute_script("return arguments[0].scrollIntoView(true);", option2)
+    option2.click()
+
+    button = browser.find_element(By.CLASS_NAME, "btn.btn-primary")
+    browser.execute_script("return arguments[0].scrollIntoView(true);", button)
+    button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
